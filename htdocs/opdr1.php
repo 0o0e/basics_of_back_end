38489ee4bcdf01a56f39bbc8842ac6e6284ed1fc
John Doe <99072495@mydavinci.nl>
2023-06-01 14:23:24 +0200

    ?

--- htdocs/opdr1.php
+++ htdocs/opdr1.php
@@ -1,39 +1,59 @@
 <?php
 $tijd = date("H:i");
+$images = ['images/morning.png','images/afternoon.png','images/evening.png','images/night.png'];
 
-function images(){
+$greetings = ['morgen','middag','avond','nacht'];
+
+// function images(){
+//     if ($GLOBALS['tijd'] >= '06:00' && $GLOBALS['tijd'] < '12:00'){
+//         return 'images/morning.png';
+//     }
+//     if($GLOBALS['tijd'] >= '12:00' && $GLOBALS['tijd'] < '18:00'){
+//         return 'images/afternoon.png';
+//     }
+//     if($GLOBALS['tijd'] >= '18:00' && $GLOBALS['tijd'] < '00:00'){
+//         return 'images/evening.png';
+//     }
+//     if($GLOBALS['tijd'] >= '00:00' && $GLOBALS['tijd'] < '06:00'){
+//         return 'images/night.png';
+//     }
+// }
+
+
+// function greeting(){
+//     if ($GLOBALS['tijd'] >= '06:00' && $GLOBALS['tijd'] < '12:00'){
+//         return 'Goede morgen!<br> Het is nu '.$GLOBALS['tijd'];
+//     }
+//     if($GLOBALS['tijd'] >= '12:00' && $GLOBALS['tijd'] < '18:00'){
+//         return 'Goede middag!<br> Het is nu '.$GLOBALS['tijd'];
+//     }
+//     if($GLOBALS['tijd'] >= '18:00' && $GLOBALS['tijd'] < '00:00'){
+//         return 'Goede avond!<br> Het is nu '.$GLOBALS['tijd'];
+//     }
+//     if($GLOBALS['tijd'] >= '00:00' && $GLOBALS['tijd'] < '06:00'){
+//         return 'Goede nacht!<br> Het is nu '.$GLOBALS['tijd'];
+//     }
+// }
+
+function dayparts(){
     if ($GLOBALS['tijd'] >= '06:00' && $GLOBALS['tijd'] < '12:00'){
-        return 'images/morning.png';
+        return 0;
     }
     if($GLOBALS['tijd'] >= '12:00' && $GLOBALS['tijd'] < '18:00'){
-        return 'images/afternoon.png';
+        return 1;
     }
     if($GLOBALS['tijd'] >= '18:00' && $GLOBALS['tijd'] < '00:00'){
-        return 'images/evening.png';
+        return 2;
     }
     if($GLOBALS['tijd'] >= '00:00' && $GLOBALS['tijd'] < '06:00'){
-        return 'images/night.png';
+        return 3;
     }
+
 }
 
-
-function greeting(){
-    if ($GLOBALS['tijd'] >= '06:00' && $GLOBALS['tijd'] < '12:00'){
-        return 'Goede morgen!<br> Het is nu '.$GLOBALS['tijd'];
-    }
-    if($GLOBALS['tijd'] >= '12:00' && $GLOBALS['tijd'] < '18:00'){
-        return 'Goede middag!<br> Het is nu '.$GLOBALS['tijd'];
-    }
-    if($GLOBALS['tijd'] >= '18:00' && $GLOBALS['tijd'] < '00:00'){
-        return 'Goede avond!<br> Het is nu '.$GLOBALS['tijd'];
-    }
-    if($GLOBALS['tijd'] >= '00:00' && $GLOBALS['tijd'] < '06:00'){
-        return 'Goede nacht!<br> Het is nu '.$GLOBALS['tijd'];
-    }
-}
-
-
-
+$nummer = dayparts();
+$image = $images[$nummer];
+$greet = "Goede $greetings[$nummer]!<br> Het is nu ".$GLOBALS['tijd'];
 ?>
 
 <!DOCTYPE html>
@@ -43,8 +63,8 @@ function greeting(){
 
         <style>
             body{
-                background-image: url('<?php echo images(); ?>');
-                background-size: 100%
+                background-image: url('<?php echo $image; ?>');
+                background-size: 100%;
             }
             .greetings{
                 margin-top: 250px;
@@ -57,7 +77,7 @@ function greeting(){
 
     </head>
     <body>
-        <h1 class = "greetings"> <?php echo greeting(); ?></h1>
+        <h1 class = "greetings"> <?php echo $greet; ?></h1>
     
         
     </body>
