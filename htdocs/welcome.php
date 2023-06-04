d158ae2f9f47a6f26f08e6d364fe684dfc4875db
John Doe <99072495@mydavinci.nl>
2023-06-04 23:34:47 +0200

    welcome.html_php_update

--- htdocs/welcome.php
+++ htdocs/welcome.php
@@ -2,8 +2,8 @@
 <body>
 
 <h1>De ingevulde gegevens zijn:</h1>
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+naam: <?php echo $_GET["naam"]; ?><br>
+email: <?php echo $_GET["email"]; ?>
 
 </body>
 </html>
